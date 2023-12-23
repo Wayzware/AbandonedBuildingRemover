71fe7362a1062431155c410e2f6345d38ee528ee
Jacob Rice <7104854+Wayzware@users.noreply.github.com>
2023-12-22 22:29:06 -0600

    Add project files.

---
+++ GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using BepInEx;
+global using HarmonyLib;
+global using System.Reflection;
+global using BepInEx.Logging;
+
+#if BEPINEX6
+global using BepInEx.Unity.Mono;
+#endif
---
+++ Systems/AbandonedBuildingRemoverSystem.cs
@@ -0,0 +1,44 @@
+﻿using Game;
+using Game.Buildings;
+using Game.Common;
+using Game.Tools;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Wayz.CS2.AbandonedBuildingRemover;
+public class AbandonedBuildingRemoverSystem : GameSystemBase
+{
+    private EntityCommandBuffer _commandBuffer;
+
+    private EntityQuery _abandonedBuildingQuery;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        _commandBuffer = base.World.GetOrCreateSystemManaged<EndFrameBarrier>().CreateCommandBuffer();
+        _abandonedBuildingQuery = GetEntityQuery(new EntityQueryDesc()
+        {
+            All =
+            [
+                ComponentType.ReadWrite<Abandoned>()
+            ],
+            None =
+            [
+                ComponentType.ReadOnly<Deleted>(),
+                ComponentType.ReadOnly<Temp>()
+            ]
+        });
+
+        RequireForUpdate(_abandonedBuildingQuery);
+    }
+
+    protected override void OnUpdate()
+    {
+
+        var abandonedBuildings = _abandonedBuildingQuery.ToEntityArray(Allocator.Temp);
+        foreach (var entity in abandonedBuildings)
+        {
+            _commandBuffer.AddComponent<Deleted>(entity);
+        }
+    }
+}
